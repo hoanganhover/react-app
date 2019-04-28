ba0081f2b774c5be1c8d660df970fc4af96760da
hoanganhover <hoanganhover@gmail.com>
2019-04-28 22:41:37 +0700

    first commit

---
+++ build/precache-manifest.a2e5f768236eb8a02405a8ad7f31ff5d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4d46f44ad693854d148",
+    "url": "/react-cart/static/js/runtime~main.641e3367.js"
+  },
+  {
+    "revision": "9444915d83e76fa6c81b",
+    "url": "/react-cart/static/js/main.42a88cda.chunk.js"
+  },
+  {
+    "revision": "4ba42f5dc6e8ef598b12",
+    "url": "/react-cart/static/js/2.fc406189.chunk.js"
+  },
+  {
+    "revision": "5663d8d67274c285a1431f80b3aa79ca",
+    "url": "/react-cart/index.html"
+  }
+];
